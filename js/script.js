6fd9d4b8fe3bd3a8175e1dd6b9e69d51a8de618c
Patrick Cypriano <patrickcyprianop@gmail.com>
2023-08-29 11:17:38 -0300

    Criação da function responsável por iniciar o tabuleiro

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const boardRegions = document.querySelectorAll("#gameBoard span");
+let vBord = [];
+let turnPlayer = "";
+
+function updateTitle() {
+  const playerInput = document.getElementById(turnPlayer);
+  document.getElementById("turnPlayer").innerText = playerInput.value;
+}
+
+function initializeGame() {
+  vBord = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  turnPlayer = "player1";
+  document.querySelector("h2").innerHTML =
+    'Vez de: <span id="turnPlayer"></span>';
+  updateTitle();
+  boardRegions.forEach(function (element) {
+    element.classList.remove("win");
+    element.innerText = "";
+    element.addEventListener("click", handleBoardClick);
+  });
+}
+
+function handleBoardClick(ev) {}
+
+document.getElementById("start").addEventListener("click", initializeGame);
